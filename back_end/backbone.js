3adc3ddd68d733691b0fb7525dc85a699d15c624
mcbridz@gmail.com <mcbridz@gmail.com>
2021-06-04 20:42:13 -0700

    initializing socket.io

--- back_end/backbone.js
+++ back_end/backbone.js
@@ -1,12 +1,40 @@
 const express = require('express')
+const mongoose = require('mongoose')
+const MongoClient = require('mongodb').MongoClient
+const cors = require('cors')
+const morgan = require('morgan')
 
 
 
 const app = express()
-const cors = require('cors')
+
+
+app.use(express.static('static'))
+app.use(express.json())
+app.use(cors())
+app.use(morgan('dev'))
 
 module.exports = function (deps) {
-
+    const mongoose = require('mongoose')
+    const url = 'mongodb://localhost/' + deps.dbname
+    mongoose.connect(url, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,        
+    })
+    app.get('/', (req, res) => {
+        res.status(200).send('Hello World!')
+    })
 
     const server = require('http').createServer(app)
+
+    const io = require("socket.io")(server, {
+        cors: {
+          origin: "http://localhost:3000",
+          methods: ["GET", "POST"]
+        }
+    })
+    
+
+    return server
 }
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const port = 8000
 const DATA_PATH = './data'
+const DB_NAME = 'yahtzee'
 
-const app = require('./back_end/backbone')({ dataPath: DATA_PATH })
+const app = require('./back_end/backbone')({ dataPath: DATA_PATH, dbname: DB_NAME })
 
 app.listen(port)
 
