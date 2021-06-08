ea756eb25794a393e787c77edc956992288df43a
mcbridz@gmail.com <mcbridz@gmail.com>
2021-06-07 19:47:15 -0700

    'debugging'

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const DB_NAME = 'yahtzee'
 const KEY = process.env.KEY || require('./secrets').key
 
 const app = require('./back_end/backbone')({ dataPath: DATA_PATH, dbname: DB_NAME, key: KEY })
-if (process.env.PORT) {
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "client", "build", "index.html"))
-    })
-}
+// if (process.env.PORT) {
+//     app.get("*", (req, res) => {
+//         res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+//     })
+// }
 app.listen(port)
 
 console.log('server listening on port: ' + port + '...')
