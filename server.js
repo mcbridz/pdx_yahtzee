8b95a4b867cd3b06854bd84135079c7d736af15e
mcbridz@gmail.com <mcbridz@gmail.com>
2021-06-07 19:43:18 -0700

    'debugging'

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const DB_NAME = 'yahtzee'
 const KEY = process.env.KEY || require('./secrets').key
 
 const app = require('./back_end/backbone')({ dataPath: DATA_PATH, dbname: DB_NAME, key: KEY })
-if (!process.env.PORT) {
+if (process.env.PORT) {
     app.get("*", (req, res) => {
         res.sendFile(path.join(__dirname, "client", "build", "index.html"))
     })
