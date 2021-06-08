d090042460389b5af159d2875b86e29cd258373c
mcbridz@gmail.com <mcbridz@gmail.com>
2021-06-07 18:51:16 -0700

    Configuring User

---
+++ back_end/protected_routes/userModel.js
@@ -0,0 +1,49 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+
+const Schema = mongoose.Schema
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    todos: []
+})
+
+userSchema.statics.signup = async function (username, plainTextPassword) {
+    const user = new this()
+    user.username = username
+    await user.hashPassword(plainTextPassword)
+    await user.save()
+    return user
+}
+
+userSchema.methods.sanitize = function () {
+    return {
+        ...this._doc,
+        password: undefined
+    }
+}
+
+userSchema.methods.hashPassword = function (plainTextPassword) {
+    const user = this
+    let randomSalt = Math.floor(Math.random() * 4 + 2)
+    return bcrypt.hash(plainTextPassword, randomSalt).then(hash => {
+        user.password = hash
+    })
+}
+
+userSchema.methods.comparePassword = function (plainTextPassword) {
+    const user = this
+    return bcrypt.compare(plainTextPassword, user.password)
+}
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = { User }
