b2f03e4954490c376c42e65353cf226010b7721a
sjohnston82 <72237516+sjohnston82@users.noreply.github.com>
2021-06-17 16:39:50 -0700

    changes to sign up user model

--- back_end/protected_routes/userModel.js
+++ back_end/protected_routes/userModel.js
@@ -1,52 +1,60 @@
-const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    emailAddress: {
-        type: String,
-        unique: true
-    }
-})
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  emailAddress: {
+    type: String,
+    unique: true,
+  },
+  firstName: {
+    type: String,
+    required: false,
+  },
+  lastName: {
+    type: String,
+    required: false,
+  },
+});
 
 userSchema.statics.signup = async function (username, plainTextPassword) {
-    const user = new this()
-    user.username = username
-    await user.hashPassword(plainTextPassword)
-    await user.save()
-    return user
-}
+  const user = new this();
+  user.username = username;
+  await user.hashPassword(plainTextPassword);
+  await user.save();
+  return user;
+};
 
 userSchema.methods.sanitize = function () {
-    return {
-        ...this._doc,
-        password: undefined
-    }
-}
+  return {
+    ...this._doc,
+    password: undefined,
+  };
+};
 
 userSchema.methods.hashPassword = function (plainTextPassword) {
-    const user = this
-    let randomSalt = Math.floor(Math.random() * 4 + 2)
-    return bcrypt.hash(plainTextPassword, randomSalt).then(hash => {
-        user.password = hash
-    })
-}
+  const user = this;
+  let randomSalt = Math.floor(Math.random() * 4 + 2);
+  return bcrypt.hash(plainTextPassword, randomSalt).then((hash) => {
+    user.password = hash;
+  });
+};
 
 userSchema.methods.comparePassword = function (plainTextPassword) {
-    const user = this
-    return bcrypt.compare(plainTextPassword, user.password)
-}
+  const user = this;
+  return bcrypt.compare(plainTextPassword, user.password);
+};
 
-const User = mongoose.model('User', userSchema)
+const User = mongoose.model("User", userSchema);
 
-module.exports = User
+module.exports = User;
--- back_end/unprotected_routes/register.js
+++ back_end/unprotected_routes/register.js
@@ -1,21 +1,27 @@
-const express = require('express')
-const jwt = require('jsonwebtoken')
-const key = process.env.KEY || require('../../secrets').key
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const key = process.env.KEY || require("../../secrets").key;
 
-const router = express.Router()
+const router = express.Router();
 
-const User = require('../protected_routes/userModel')
+const User = require("../protected_routes/userModel");
 
-router.post('/register', (req, res) => {
-    console.log(`Looking for user ${req.body.username}`)
-    User.findOne({ username: req.body.username }, async (err, userExists) => {
-        console.log('finished searching for username')
-        if (err) return res.status(500).send(err)
-        if (userExists) return res.status(400).send('username already exists')
-        console.log('creating user')
-        const user = await User.signup(req.body.username, req.body.password)
-        res.status(201).send(user.sanitize())
-    })
-})
+router.post("/register", (req, res) => {
+  console.log(`Looking for user ${req.body.username}`);
+  User.findOne({ username: req.body.username }, async (err, userExists) => {
+    console.log("finished searching for username");
+    if (err) return res.status(500).send(err);
+    if (userExists) return res.status(400).send("username already exists");
+    console.log("creating user");
+    const user = await User.signup(
+      req.body.username,
+      req.body.password,
+      req.body.email,
+      req.body.firstName,
+      req.body.lastName
+    );
+    res.status(201).send(user.sanitize());
+  });
+});
 
-module.exports = router
+module.exports = router;
