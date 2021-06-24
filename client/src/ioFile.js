04eb50e72dc5451f9839a3ebec39834c911310fa
mcbridz@gmail.com <mcbridz@gmail.com>
2021-06-23 18:34:05 -0700

    Heroku

--- client/src/ioFile.js
+++ client/src/ioFile.js
@@ -1,13 +1,9 @@
-import io from "socket.io"; //HEROKU
-// import io from "socket.io-client"; //DEVELOPMENT
+// import io from "socket.io"; //HEROKU
+import io from "socket.io-client"; //DEVELOPMENT
 var output;
 if (process.env.NODE_ENV === "production") {
   console.log("Creating Heroku Socket")
   output = io();
-  output.on("test", (data) => {
-    console.log(data)
-  })
-  output.emit("test", "test")
   console.log(output)
   console.log(output.connected)
 } else {
