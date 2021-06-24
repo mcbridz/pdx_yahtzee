475cb27fd2a162bdd56ac337929ca4f4c3854b14
mcbridz@gmail.com <mcbridz@gmail.com>
2021-06-23 18:17:42 -0700

    Heroku

--- client/src/ioFile.js
+++ client/src/ioFile.js
@@ -2,11 +2,13 @@ import io from "socket.io"; //HEROKU
 // import io from "socket.io-client"; //DEVELOPMENT
 var output;
 if (process.env.NODE_ENV === "production") {
-    console.log("Creating Heroku Socket")
-    output = io();
-  } else {
-    console.log('CREATING SOCKET')
-    output = io("http://localhost:8000", { transports: ["websocket"] });
+  console.log("Creating Heroku Socket")
+  output = io();
+  console.log(output)
+  console.log(output.connected)
+} else {
+  console.log('CREATING SOCKET')
+  output = io("http://localhost:8000", { transports: ["websocket"] });
 }
 const socket = output
 
